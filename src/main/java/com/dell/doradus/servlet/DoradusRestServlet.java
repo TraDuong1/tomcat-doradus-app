95d77346fb50ac966f9e3cfae39c8ea9baad5dd2
Tra Duong <tra.duong@software.dell.com>
2015-02-10 23:07:15 -0800

    Tomcat app to use Doradus Service

--- src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
+++ src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
@@ -5,21 +5,25 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.dell.doradus.service.rest.RESTServlet;
+
 @WebServlet(name = "DoradusRestServlet", urlPatterns = {"/*"})
-public class DoradusRestServlet extends HttpServlet {
+public class DoradusRestServlet extends RESTServlet {
 	private static final long serialVersionUID = 4815487822036229036L;
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println(request.getPathInfo());
+		super.doPost(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
     	out.println(request.getPathInfo());
+    	super.doGet(request, response);
+    	
    }	
 //	private static final String[] SERVICES = new String[]{
 //	        SpiderService.class.getName(),
