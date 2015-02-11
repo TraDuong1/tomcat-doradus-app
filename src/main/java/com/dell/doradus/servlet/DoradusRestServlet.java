05de0f547407c5cdc8007a14b61cff1a5092209a
Tra Duong <tra.duong@software.dell.com>
2015-02-10 23:19:29 -0800

    Tomcat app to use Doradus Service

--- src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
+++ src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
@@ -3,12 +3,16 @@ package com.dell.doradus.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.dell.doradus.core.DoradusServer;
+import com.dell.doradus.service.olap.OLAPService;
 import com.dell.doradus.service.rest.RESTServlet;
+import com.dell.doradus.service.spider.SpiderService;
 
 @WebServlet(name = "DoradusRestServlet", urlPatterns = {"/*"})
 public class DoradusRestServlet extends RESTServlet {
@@ -25,15 +29,15 @@ public class DoradusRestServlet extends RESTServlet {
     	//super.doGet(request, response);
     	
    }	
-//	private static final String[] SERVICES = new String[]{
-//	        SpiderService.class.getName(),
-//	        OLAPService.class.getName()
-//	        //RESTService.class.getName()
-//	};
-//	 
-//	@Override
-//	public void init(ServletConfig config)  {
-//		final String[] args = new String[] { "-dbhost", "10.228.23.117", "-dbport", "9042", "-restport", "57631", "-dbuser", "SuperDory", "-dbpassword", "Alpha1"};
-//				DoradusServer.startEmbedded(args, SERVICES);
-//	}
+	private static final String[] SERVICES = new String[]{
+	        SpiderService.class.getName(),
+	        OLAPService.class.getName()
+	        //RESTService.class.getName()
+	};
+	 
+	@Override
+	public void init(ServletConfig config)  {
+		final String[] args = new String[] { "-dbhost", "10.228.23.117", "-dbport", "9042", "-dbuser", "SuperDory", "-dbpassword", "Alpha1"};
+				DoradusServer.startEmbedded(args, SERVICES);
+	}
 }
