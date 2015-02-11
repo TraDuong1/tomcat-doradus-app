22df50a9105d04e56f17099b5c8964287647b245
Tra Duong <tra.duong@software.dell.com>
2015-02-10 23:09:44 -0800

    Tomcat app to use Doradus Service

--- src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
+++ src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
@@ -16,13 +16,13 @@ public class DoradusRestServlet extends RESTServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println(request.getPathInfo());
-		super.doPost(request, response);
+		//super.doPost(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
     	out.println(request.getPathInfo());
-    	super.doGet(request, response);
+    	//super.doGet(request, response);
     	
    }	
 //	private static final String[] SERVICES = new String[]{
