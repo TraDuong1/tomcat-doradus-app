30e17a7a5d296f2a79d826f89dff4c9b7f76442c
Tra Duong <tra.duong@software.dell.com>
2015-02-12 15:50:44 -0800

    Tomcat app to use Doradus Service

--- src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
+++ src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
@@ -2,6 +2,10 @@ package com.dell.doradus.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -9,8 +13,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.cassandra.utils.Pair;
+
+import com.dell.doradus.common.Utils;
 import com.dell.doradus.core.DoradusServer;
 import com.dell.doradus.service.olap.OLAPService;
+import com.dell.doradus.service.rest.NotFoundException;
+import com.dell.doradus.service.rest.RESTCommand;
+import com.dell.doradus.service.rest.RESTService;
 import com.dell.doradus.service.rest.RESTServlet;
 import com.dell.doradus.service.spider.SpiderService;
 
@@ -20,12 +30,23 @@ public class DoradusRestServlet extends RESTServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println(request.getPathInfo());
-		//super.doPost(request, response);
+		doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
     	out.println(request.getPathInfo());
+        Map<String, String> variableMap = new HashMap<String, String>();
+        String query = extractQueryParam(request, variableMap);
+        RESTCommand cmd = RESTService.instance().matchCommand(request.getMethod(),
+                                                              request.getRequestURI(),
+                                                              query,
+                                                              variableMap);
+        if (cmd == null) {
+            throw new NotFoundException("Request does not match a known URI: " + request.getRequestURL());
+        }
+        Utils.require(cmd != null, "Request does not match a known URI: " + request.getRequestURL());
+        out.println("Command: {}" + cmd.toString());
     	//super.doGet(request, response);
     	
    }	
@@ -40,4 +61,71 @@ public class DoradusRestServlet extends RESTServlet {
 		final String[] args = new String[] { "-dbhost", "10.228.23.117", "-dbport", "9042", "-dbuser", "SuperDory", "-dbpassword", "Alpha1"};
 				DoradusServer.startEmbedded(args, SERVICES);
 	}
+	
+	   private String extractQueryParam(HttpServletRequest request, Map<String, String> restParams) {
+	        String query = request.getQueryString();
+	        if (Utils.isEmpty(query)) {
+	            return "";
+	        }
+	        StringBuilder buffer = new StringBuilder(query);
+	        
+	        // Split query component into decoded, &-separate components.
+	        String[] parts = Utils.splitURIQuery(buffer.toString());
+	        List<Pair<String, String>> unusedList = new ArrayList<Pair<String,String>>();
+	        boolean bRewrite = false;
+	        for (String part : parts) {
+	            Pair<String, String> param = extractParam(part);
+	            switch (param.left.toLowerCase()) {
+	            case "api":
+	                bRewrite = true;
+	                restParams.put("api", param.right);
+	                break;
+	            case "format":
+	                bRewrite = true;
+	                if (param.right.equalsIgnoreCase("xml")) {
+	                    restParams.put("format", "text/xml");
+	                } else if (param.right.equalsIgnoreCase("json")) {
+	                    restParams.put("format", "application/json");
+	                }
+	                break;
+	            case "tenant":
+	                bRewrite = true;
+	                restParams.put("tenant", param.right);
+	                break;
+	            default:
+	                unusedList.add(param);
+	            }
+	        }
+	        
+	        // If we extracted any fixed params, rewrite the query parameter.
+	        if (bRewrite) {
+	            buffer.setLength(0);
+	            for (Pair<String, String> pair : unusedList) {
+	                if (buffer.length() > 0) {
+	                    buffer.append("&");
+	                }
+	                buffer.append(Utils.urlEncode(pair.left));
+	                if (pair.right != null) {
+	                    buffer.append("=");
+	                    buffer.append(Utils.urlEncode(pair.right));
+	                }
+	            }
+	        }
+	        return buffer.toString();
+	    }   // extractQueryParam
+
+	    // Split the given k=v (or just k) param into a Pair object.
+	    private Pair<String, String> extractParam(String part) {
+	        int eqInx = part.indexOf('=');
+	        String paramName;
+	        String paramValue;
+	        if (eqInx < 0) {
+	            paramName = part;
+	            paramValue = null;
+	        } else {
+	            paramName = part.substring(0, eqInx);
+	            paramValue = part.substring(eqInx + 1);
+	        }
+	        return Pair.create(paramName, paramValue);
+	    }   // extractParam	
 }
