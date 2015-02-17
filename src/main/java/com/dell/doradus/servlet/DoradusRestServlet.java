3f059783e83c11aec4502513024dd64c4e51158c
Tra Duong <tra.duong@software.dell.com>
2015-02-17 14:32:54 -0800

    Tomcat app to use Doradus Service

--- src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
+++ src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
@@ -20,7 +20,7 @@ import com.dell.doradus.service.spider.SpiderService;
 /**
  * Servlet implementation class DoradusRestServlet
  */
-@WebServlet(name = "DoradusRestServlet", urlPatterns = {"/*"})
+@WebServlet(name = "DoradusRestServlet", urlPatterns = {"/doradus/*"})
 public class DoradusRestServlet extends RESTServlet {
 	private static final long serialVersionUID = 1L;
 	 private final Logger logger = LoggerFactory.getLogger(getClass().getSimpleName());
