4cdd10ce0202b21a5b95f60cd4ab6336a751ebfb
Tra Duong <tra.duong@software.dell.com>
2015-02-17 12:39:42 -0800

    Tomcat app to use Doradus Service

--- src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
+++ src/main/java/com/dell/doradus/servlet/DoradusRestServlet.java
@@ -1,66 +1,65 @@
 package com.dell.doradus.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.dell.doradus.core.DoradusServer;
 import com.dell.doradus.service.olap.OLAPService;
-import com.dell.doradus.service.rest.RESTCommand;
-import com.dell.doradus.service.rest.RESTService;
 import com.dell.doradus.service.rest.RESTServlet;
 import com.dell.doradus.service.spider.SpiderService;
 
+/**
+ * Servlet implementation class DoradusRestServlet
+ */
 @WebServlet(name = "DoradusRestServlet", urlPatterns = {"/*"})
 public class DoradusRestServlet extends RESTServlet {
-	private static final long serialVersionUID = 4815487822036229036L;
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-		out.println(request.getPathInfo());
-		//super.doPost(request, response);
+	private static final long serialVersionUID = 1L;
+    private final Logger logger = LoggerFactory.getLogger(getClass().getSimpleName());       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoradusRestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out = response.getWriter();
-    	out.println(request.getPathInfo());
-       	out.println("getRequestURI: "+ request.getRequestURI());        	
-       	out.println("getMethod: "+ request.getMethod());         	
-       	out.println("query: "+ request.getQueryString());
-        Map<String, String> variableMap = new HashMap<String, String>();
-        try {
-        String query = request.getQueryString();
-        RESTCommand cmd = RESTService.instance().matchCommand(request.getMethod(),
-                request.getRequestURI(),
-                query,
-                variableMap);
-       		out.println("cmd: "+  cmd.toString());
-        } catch(Throwable e) {
-        	StringBuilder sb = new StringBuilder();
-            for (StackTraceElement element : e.getStackTrace()) {
-                sb.append(element.toString());
-                sb.append("\n");
-            }
-        	out.println("stacktrace: "+  sb.toString());
-        }
-    	//super.doGet(request, response);
-    	
-   }	
-	private static final String[] SERVICES = new String[]{
-	        SpiderService.class.getName(),
-	        OLAPService.class.getName()
-	        //RESTService.class.getName()
-	};
-	 
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	logger.debug("getPathInfo: " + request.getPathInfo());
+    	logger.debug("getRequestURI: "+ request.getRequestURI());        	
+    	logger.debug("getMethod: "+ request.getMethod());         	
+    	logger.debug("getQueryString: "+ request.getQueryString());
+       	super.doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
 	@Override
 	public void init(ServletConfig config)  {
-		final String[] args = new String[] { "-dbhost", "10.228.23.117", "-dbport", "9042", "-dbuser", "SuperDory", "-dbpassword", "Alpha1"};
+
+		final String[] args = new String[] { "-dbhost", "localhost", "-dbport", "9160"};
 				DoradusServer.startEmbedded(args, SERVICES);
+		logger.debug("doradus started... ");	
 	}
+	
+	private static final String[] SERVICES = new String[]{
+        SpiderService.class.getName(),
+        OLAPService.class.getName()
+        //RESTService.class.getName()
+};
 }
